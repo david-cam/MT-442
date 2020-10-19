5d34eb0026d22c16bf24831a002fe7d2b751f6ca
david-cam <davidcam@buffalo.edu>
2020-10-19 04:39:56 -0400

    Changed Port #

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const { response } = require('express');
-const port = 3000;
+const port = process.env.PORT || '3000'';
 
 const app = express();
+app.set("port", PORT);
+
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.static('static'))
 
