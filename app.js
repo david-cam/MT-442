2f97a5b99b6513a65a85bb883c4269dc17a60aaf
david-cam <davidcam@buffalo.edu>
2020-11-09 14:21:18 -0500

    hotfix server

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const path = require('path');
 const bodyParser = require('body-parser')
 //const connection = require("./model");
 const mongoose =require('mongoose');
-const port = process.env.port || 3000 ;
+const port = process.env.PORT || 3000 ;
 const app = express();
+
 
 //define the modules we use
 const bcrypt = require('bcryptjs')
