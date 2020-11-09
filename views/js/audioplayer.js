0cdf2fbde33259c2635959c00841dbded1d96cc5
david-cam <davidcam@buffalo.edu>
2020-11-09 15:22:57 -0500

    implement playTunes

--- views/js/audioplayer.js
+++ views/js/audioplayer.js
@@ -11,3 +11,29 @@ function playSong(songName){
     artistName.innerHTML = artistNameHTML;
 
 }
+
+
+    function playTunes() {
+      var request = new XMLHttpRequest();
+      request.open("GET", "http://localhost:3000/audio/", true);
+      request.responseType = "arraybuffer";
+
+     // spinner.show();
+
+      request.onload = function() {
+     //     spinner.hide();
+        var Data = request.response;
+        process(Data);
+      };
+
+      request.send();
+    }
+
+    function process(Data) {
+      source = context.createBufferSource(); // Create Sound Source
+      context.decodeAudioData(Data, function(buffer) {
+        source.buffer = buffer;
+        source.connect(context.destination);
+        source.start(context.currentTime);
+      });
+    }
